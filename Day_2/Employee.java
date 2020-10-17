3f2eb08dc151d20376cca8e5fda3cb735f1e71d9
ratnadeepikavuddagiri <50200336+ratnadeepikavuddagiri@users.noreply.github.com>
2020-10-17 20:34:22 +0530

    Add files via upload

---
+++ Day_2/Employee.java
@@ -0,0 +1,14 @@
+package Day_2;
+
+public class Employee {
+	String name;
+	String city;
+	int age;
+	
+	
+	public void display(){
+		System.out.println("The name is "+name);
+		System.out.println("The age is "+age);
+		System.out.println("The city is "+city);
+	}
+}
---
+++ Day_2/Main.java
@@ -0,0 +1,19 @@
+package Day_2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e1 =new Employee();
+		Employee e2 =new Employee();
+		e1.name="Saurab";
+		e1.age=23;
+		e1.city="chennai";
+		e1.display();
+		e2.name = "Gupta";
+		e2.age=20;
+		e2.city="Banglore";
+		
+	}
+
+}
